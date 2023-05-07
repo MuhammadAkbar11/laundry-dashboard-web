d1fca462aa2323ab3604c99878263e4eab6a0e21
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-05-08 00:14:26 +0700

    chore: Added a varsConfig.ts file that contains constant variables for application configuration

---
+++ src/configs/varsConfig.ts
@@ -0,0 +1,17 @@
+import { BootstrapBreakpointsTypes } from '@utils/types';
+
+export const UNKNOWM_ERROR = 'UNKNOWM_ERROR';
+export const API_URI = process.env.NEXT_PUBLIC_API_URI;
+
+export const MAX_AVATAR_SIZE = 5000000;
+export const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
+
+export const BOOTSTRAP_BREAKPOINTS: Record<BootstrapBreakpointsTypes, string> =
+  {
+    xs: '(max-width: 576px)',
+    sm: '(min-width: 576px)',
+    md: '(min-width: 768px)',
+    lg: '(min-width: 992px)',
+    xl: '(min-width: 1200px)',
+    xxl: '(min-width: 1400px)',
+  };

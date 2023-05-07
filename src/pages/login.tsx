9b4faf1421550713139b4b4f837b7cf99329f04b
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-05-08 00:19:32 +0700

    refactor: change sign-in page to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,26 +3,35 @@ import React from 'react';
 import Head from 'next/head';
 import { Card, Col, Row } from 'react-bootstrap';
 import AuthLayout from '@layouts/AuthLayout';
-import Image from 'next/image';
+// import Image from 'next/image';
 import FormSignIn from '@components/Forms/FormSignIn';
+import { GetServerSidePropsContext } from 'next';
+import { isAuthenticadedService } from '@/services/authSevices';
+import { uIsAuthRedirect } from '@utils/utils';
 
-export default function PagesSignIn() {
+type Props = {
+  // userAuth: IUserAuth;
+};
+
+export default function PagesSignIn({}: Props) {
+  // console.log(userAuth);
+
   return (
     <>
       <Head>
-        <title>Sign In | AdminKit Demo</title>
+        <title>Masuk | AdminKit Demo</title>
       </Head>
       <Row className=" vh-100">
         <Col sm={10} md={8} lg={6} className=" mx-auto d-table h-100">
           <div className="d-table-cell align-middle">
             <div className="text-center mt-4">
-              <h1 className="h2">Welcome back, Charles</h1>
-              <p className="lead">Sign in to your account to continue</p>
+              <h1 className="h2">Selamat datang</h1>
+              <p className="lead">Masuk ke akun Anda untuk melanjutkan</p>
             </div>
             <Card>
               <Card.Body>
                 <div className="m-sm-4">
-                  <div className="text-center">
+                  {/* <div className="text-center">
                     <Image
                       src="/img/avatars/avatar.jpg"
                       alt="Charles Hall"
@@ -31,7 +40,7 @@ export default function PagesSignIn() {
                       height={132}
                       priority
                     />
-                  </div>
+                  </div> */}
                   <FormSignIn />
                 </div>
               </Card.Body>
@@ -41,6 +50,20 @@ export default function PagesSignIn() {
       </Row>
     </>
   );
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const userAgent = ctx.req.headers['user-agent'];
+  const cookies = ctx.req.headers.cookie;
+  const isAuth = await isAuthenticadedService({
+    headers: { Cookie: cookies, 'User-Agent': userAgent },
+  });
+
+  if (isAuth) return uIsAuthRedirect();
+
+  return {
+    props: {},
+  };
 }
 
 PagesSignIn.layout = AuthLayout;

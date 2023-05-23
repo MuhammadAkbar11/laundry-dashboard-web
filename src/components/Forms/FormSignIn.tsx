8e1801271222b5c9adff345d4a963d2cfb03d9c2
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-05-23 23:28:21 +0700

    refactor: Refactor FormSignIn component
    Refactor the FormSignIn component to improve code structure, readability, and performance.
    
    - Added necessary imports for dependencies and custom hooks.
    - Updated the default values for email and password fields based on the NODE_ENV.
    - Implemented form submission using the useMutation hook.
    - Added route change handlers for better user experience.
    - Added success and error notifications for login.
    - Updated labels and placeholders for email and password fields.
    
    This refactoring enhances the maintainability and efficiency of the FormSignIn component.

--- src/components/Forms/FormSignIn.tsx
+++ src/components/Forms/FormSignIn.tsx
@@ -1,20 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SignInInputTypes, signInSchema } from '@utils/schema/authSchema';
 import { useRouter } from 'next/router';
+import { useMutation } from '@tanstack/react-query';
+import { postSignInService } from '@/services/authSevices';
+import useNotification from '@hooks/useNotification';
+import useRouteChangeHandlers from '@hooks/useRouteChangeHandlers';
+import { NODE_ENV } from '@configs/varsConfig';
 
 type Props = {};
 
 function FormSignIn({}: Props) {
+  const [showPassword, setShowPassword] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const router = useRouter();
+  const notif = useNotification();
 
   const methods = useForm<SignInInputTypes>({
     resolver: zodResolver(signInSchema),
     defaultValues: {
-      email: 'adminkit@adminkit.io',
+      email: NODE_ENV === 'development' ? 'superadmin@gmail.com' : '',
+      password: NODE_ENV === 'development' ? '123456' : '',
     },
   });
 
@@ -24,18 +33,43 @@ function FormSignIn({}: Props) {
     formState: { errors },
   } = methods;
 
-  const onSubmitHandler = (inputs: SignInInputTypes) => {
-    setLoading(true);
-    // eslint-disable-next-line no-console
-    console.log(inputs);
-    router.push('/');
-  };
+  const mutation = useMutation(postSignInService);
+  useRouteChangeHandlers({
+    routeChangeComplete: () => {
+      setLoading(false);
+    },
+    routeChangeError: () => {
+      setLoading(false);
+    },
+    unsubRouteChangeComplete: false,
+    unsubRouteChangeError: false,
+  });
 
   React.useEffect(() => {
-    return () => {
-      setLoading(false);
-    };
-  }, []);
+    const redirect = router.query?.redirect?.toString() || '/';
+
+    if (mutation.isSuccess) {
+      router.push(redirect);
+    }
+  }, [mutation.isSuccess, router]);
+
+  const onSubmitHandler = (inputs: SignInInputTypes) => {
+    setLoading(true);
+    mutation.mutate(inputs, {
+      onSuccess(data) {
+        notif.success(data?.message || 'Login berhasil', { duration: 10000 });
+      },
+      onError(error: any) {
+        setLoading(false);
+        // eslint-disable-next-line no-console
+        console.log(error);
+        const errMessage = error?.name?.includes('AUTH')
+          ? error?.message
+          : 'Login gagal! silahkan coba lagi';
+        notif.danger(errMessage);
+      },
+    });
+  };
 
   return (
     <Form method="POST" onSubmit={handleSubmit(onSubmitHandler)}>
@@ -45,7 +79,7 @@ function FormSignIn({}: Props) {
           size="lg"
           type="email"
           id="email"
-          placeholder="Enter your email"
+          placeholder="Masukan email"
           isInvalid={!!errors?.email}
           {...register('email')}
         />
@@ -56,12 +90,12 @@ function FormSignIn({}: Props) {
         ) : null}
       </div>
       <div className="mb-3">
-        <Form.Label htmlFor="password">Password</Form.Label>
+        <Form.Label htmlFor="password">Kata Sandi</Form.Label>
         <Form.Control
           size="lg"
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           id="password"
-          placeholder="Enter your password"
+          placeholder="Masukan kata sandi"
           isInvalid={!!errors?.password}
           autoComplete="on"
           {...register('password')}
@@ -71,21 +105,21 @@ function FormSignIn({}: Props) {
             {errors?.password?.message}
           </Form.Control.Feedback>
         ) : null}
-        <small>
-          <a href="index.html">Forgot password?</a>
-        </small>
-      </div>
-      <div>
-        <label className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            defaultValue="remember-me"
-            name="remember-me"
-            defaultChecked
-          />
-          <span className="form-check-label">Remember me next time</span>
-        </label>
+        <div className="mt-2">
+          <label className="form-check" style={{ cursor: 'pointer' }}>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              defaultValue="remember-me"
+              name="remember-me"
+              onChange={() => {
+                setShowPassword((prevSt) => !prevSt);
+              }}
+              defaultChecked={showPassword}
+            />
+            <span className="form-check-label">Tampilkan kata sandi?</span>
+          </label>
+        </div>
       </div>
       <div className="text-center mt-3">
         <button
@@ -93,7 +127,7 @@ function FormSignIn({}: Props) {
           className="btn btn-lg btn-primary"
           disabled={loading}
         >
-          Sign in
+          Masuk
         </button>
         {/* <button type="submit" class="btn btn-lg btn-primary">Sign in</button> */}
       </div>

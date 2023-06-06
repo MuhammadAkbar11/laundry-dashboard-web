e99254a09e3181a6bc7ec2bba30d0bab451261f8
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-06-07 02:03:37 +0700

    refactor: Add custom 404 page with localized content
    Add custom 404 page (404.tsx) with localized content and improved UI. The modifications include:
    
    - Importing the BoxButton component from '@components/Buttons/BoxButton' for a custom styled button.
    - Updating the page content to display localized text in Indonesian
    - Replacing the previous "Return to website" button with a new styled button that navigates to the home page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MiscLayout from '@/layouts/MiscLayout';
 import Link from 'next/link';
 import Head from 'next/head';
+import BoxButton from '@components/Buttons/BoxButton';
 
 type Props = {};
 
@@ -18,12 +19,15 @@ function Custom404({}: Props) {
               <div className="d-table-cell align-middle">
                 <div className="text-center">
                   <h1 className="display-1 font-weight-bold">404</h1>
-                  <p className="h1">Page not found.</p>
-                  <p className="h2 font-weight-normal mt-3 mb-4">
-                    The page you are looking for might have been removed.
+                  <p className="h1">Halaman tidak ditemukan</p>
+                  <p className="h3 font-weight-normal text-gray mt-3 mb-4">
+                    Halaman yang Anda cari tidak dapat di temukan atau mungkin
+                    telah dihapus!{' '}
                   </p>
-                  <Link href="/" className="btn btn-primary btn-lg">
-                    Return to website
+                  <Link href="/" passHref legacyBehavior>
+                    <BoxButton icon="Home" size="lg" className="py-2">
+                      Kembali ke halaman dashboard
+                    </BoxButton>
                   </Link>
                 </div>
               </div>

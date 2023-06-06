50a41080dc5a40534ea72dc15d15128bc87105d8
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-06-06 22:27:26 +0700

    refactor: Remove unused code and comments in useGetCustomerLevels hook

--- src/utils/hooks/useGetCustomerLevels.tsx
+++ src/utils/hooks/useGetCustomerLevels.tsx
@@ -1,15 +1,10 @@
 import { getCustomerLevelsService } from '@/services/customerService';
 import { useQuery } from '@tanstack/react-query';
-// import React from 'react';
-
-// type Props = {};
 
 const useGetCustomerLevels = () =>
   useQuery({
     queryKey: ['customerLevelsData'],
     queryFn: () => getCustomerLevelsService(),
   });
-// const useGetCustomerLevels = () =>
-//   useQuery('customerLevels', () => getCustomerLevelsService());
 
 export default useGetCustomerLevels;

76de6c9c72623c3f97ced56317a1b39c687db1e1
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-07-18 02:35:41 +0700

    feat(configs): Add pageConfigs module
    Created a new file: src/configs/pageConfigs.ts. This module provides page configuration data used for navigation and permission management.

---
+++ src/configs/pageConfigs.ts
@@ -0,0 +1,43 @@
+import navigationConfigs from './navigationConfigs';
+
+interface IPageConfig {
+  path: string;
+  permissions?: string[];
+  isDynamicPage?: boolean;
+}
+
+const permissionNavigationConfigs: IPageConfig[] = Object.values(
+  navigationConfigs
+).reduce<IPageConfig[]>((acc, { navItems }) => {
+  const filteredNavItems = navItems.reduce((items, item) => {
+    const { href, permissions } = item;
+    if (href && permissions) {
+      items.push({ path: href, permissions });
+    }
+
+    if (item.navSubItems) {
+      const filteredSubItems = item.navSubItems.reduce((subItems, subItem) => {
+        const { href: subHref, permissions: subPermissions } = subItem;
+        if (subHref && subPermissions) {
+          subItems.push({ path: subHref, permissions: subPermissions });
+        }
+        return subItems;
+      }, [] as IPageConfig[]);
+      items.push(...filteredSubItems);
+    }
+
+    return items;
+  }, [] as IPageConfig[]);
+
+  acc.push(...filteredNavItems);
+  return acc;
+}, []);
+
+const pagesConfigs: IPageConfig[] = [
+  ...permissionNavigationConfigs,
+  { path: '/admin/profile', permissions: ['*'] },
+  { path: '/admin/laundry/room/', permissions: ['*'], isDynamicPage: true },
+  { path: '/admin/laundry/bayar/', permissions: ['*'], isDynamicPage: true },
+];
+
+export default pagesConfigs;

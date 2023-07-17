675de6eb11bffb9d1d342cd6f5ebff2f4945bdc8
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-07-18 02:51:32 +0700

    fix(components/badge): update LQPayStatusBadge and LQStatusBadge
    - Modify the file `src/components/Badges/LQPayStatusBadge.tsx`:
      - Add imports for `FontAwesomeIcon` and relevant icons from `@fortawesome/free-solid-svg-icons`
      - Update the component logic to use `FontAwesomeIcon` for icon rendering based on payment status
      - Adjust CSS classes and conditionally apply background colors based on the payment status
    
    - Modify the file `src/components/Badges/LQStatusBadge.tsx`:
      - Add props for `onClick`, `endIcon`, and `disabled`
      - Update the component logic to utilize the provided props and apply proper styling and icons based on the status
      - Set default prop values for `onClick`, `endIcon`, and `disabled`

--- src/components/Badges/LQPayStatusBadge.tsx
+++ src/components/Badges/LQPayStatusBadge.tsx
@@ -1,5 +1,7 @@
 import { Badge } from 'react-bootstrap';
 import clsx from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDotCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 type LQPayStatusBadgeProps = {
   value: string;
@@ -9,6 +11,8 @@ function LQPayStatusBadge({ value }: LQPayStatusBadgeProps): JSX.Element {
   const clsNm = clsx('p-2 rounded-0 d-flex align-items-center', {
     'bg-success': value?.trim() === 'FINISHED',
     'bg-warning text-dark': value?.trim() === 'PENDING',
+    'bg-danger': value?.trim() === 'REJECTED',
+    'bg-info': value?.trim() === 'PROCESSED',
   });
 
   let content: JSX.Element;
@@ -16,18 +20,70 @@ function LQPayStatusBadge({ value }: LQPayStatusBadgeProps): JSX.Element {
   if (value === 'FINISHED') {
     content = (
       <Badge className={clsNm}>
-        <span className="p-1 bg-white rounded-circle " />
-        <span className="ms-1">Sudah Bayar</span>
+        <FontAwesomeIcon
+          icon={faDotCircle}
+          style={{
+            fontSize: 8,
+          }}
+          className="bg-white rounded-circle"
+        />
+        <span className="ms-1">Lunas</span>
+      </Badge>
+    );
+  } else if (value === 'REJECTED') {
+    content = (
+      <Badge className={clsNm}>
+        <FontAwesomeIcon
+          style={{
+            fontSize: 8,
+          }}
+          icon={faTimesCircle}
+          className=" bg-white rounded-circle"
+        />
+        <span className="ms-1">Ditolak</span>
+      </Badge>
+    );
+  } else if (value === 'PENDING') {
+    content = (
+      <Badge className={clsNm}>
+        <FontAwesomeIcon
+          style={{
+            fontSize: 8,
+          }}
+          icon={faDotCircle}
+          className=" bg-dark rounded-circle"
+        />
+        <span className="ms-1">Belum Bayar</span>
+      </Badge>
+    );
+  } else if (value === 'PROCESSED') {
+    content = (
+      <Badge className={clsNm}>
+        <FontAwesomeIcon
+          style={{
+            fontSize: 8,
+          }}
+          icon={faDotCircle}
+          className=" bg-dark rounded-circle"
+        />
+        <span className="ms-1">Diproses</span>
       </Badge>
     );
   } else {
     content = (
       <Badge className={clsNm}>
-        {/* <FeatherIcon name="Circle" className="" size={12} /> */}
-        <span className="p-1 bg-dark rounded-circle " />
-        <span className="ms-1">Belum Bayar</span>
+        <FontAwesomeIcon
+          style={{
+            fontSize: 8,
+          }}
+          icon={faDotCircle}
+          className=" bg-secondary rounded-circle"
+        />
+        <span className="ms-1 text-capitalize">
+          {value.toLocaleLowerCase()}
+        </span>
       </Badge>
-    );
+    ); // Add your fallback content here if needed
   }
 
   return <div className="d-inline-block">{content}</div>;
--- src/components/Badges/LQStatusBadge.tsx
+++ src/components/Badges/LQStatusBadge.tsx
@@ -5,41 +5,102 @@ import FeatherIcon from '@components/Icons/FeatherIcon';
 
 type LQStatusBadgeProps = {
   value: string;
+  onClick?: (e: React.MouseEvent) => void;
+  endIcon?: React.ReactNode;
+  disabled?: boolean;
 };
 
-function LQStatusBadge({ value }: LQStatusBadgeProps): JSX.Element {
-  const clsNm = clsx('p-2 rounded-0 d-flex align-items-center', {
+function LQStatusBadge({
+  value,
+  onClick,
+  endIcon,
+  disabled,
+}: LQStatusBadgeProps): JSX.Element {
+  const clsNm = clsx('p-2 rounded-0 border-0 d-flex align-items-center', {
     'bg-primary': value === 'WASHED',
     'bg-success': value === 'FINISHED',
-    'bg-warning text-dark': value === 'ONHOLD',
+    'bg-blue': value === 'ONHOLD',
+    'bg-warning text-dark': value === 'PENDING',
+    'bg-danger': value === 'CANCELED',
+    'disabled opacity-75': disabled,
   });
 
   let content: JSX.Element;
 
   if (value === 'ONHOLD') {
     content = (
-      <Badge className={clsNm}>
-        <FeatherIcon name="Upload" size={12} />
+      <Badge
+        as="button"
+        disabled={disabled}
+        className={clsNm}
+        onClick={onClick}
+      >
+        <FeatherIcon name="Activity" size={12} />
         <span className="ms-1">Proses</span>
+        {endIcon}
       </Badge>
     );
   } else if (value === 'FINISHED') {
     content = (
-      <Badge className={clsNm}>
+      <Badge
+        as="button"
+        disabled={disabled}
+        className={clsNm}
+        onClick={onClick}
+      >
         <FeatherIcon name="CheckCircle" size={12} />
         <span className="ms-1">Selesai</span>
+        {endIcon}
+      </Badge>
+    );
+  } else if (value === 'PENDING') {
+    content = (
+      <Badge
+        as="button"
+        disabled={disabled}
+        className={clsNm}
+        onClick={onClick}
+      >
+        <FeatherIcon name="MinusCircle" size={12} />
+        <span className="ms-1">Menunggu</span>
+        {endIcon}
+      </Badge>
+    );
+  } else if (value === 'CANCELED') {
+    content = (
+      <Badge
+        as="button"
+        disabled={disabled}
+        className={clsNm}
+        onClick={onClick}
+      >
+        <FeatherIcon name="XCircle" size={12} />
+        <span className="ms-1">DiBatalkan</span>
+        {endIcon}
       </Badge>
     );
   } else {
     content = (
-      <Badge className={clsNm}>
+      <Badge
+        as="button"
+        disabled={disabled}
+        className={clsNm}
+        onClick={onClick}
+      >
         <FeatherIcon name="RefreshCcw" size={12} />
         <span className="ms-1">Cuci</span>
+        {endIcon}
       </Badge>
     );
   }
 
   return <div className="d-inline-block">{content}</div>;
 }
+
+LQStatusBadge.defaultProps = {
+  onClick: () => null,
+  endIcon: null,
+  disabled: true,
+};
 
 export default LQStatusBadge;

4f4828145818709877d6186fd8852a2682dadb3e
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-08-07 02:24:31 +0700

    refactor(utils): Update axios headers
    - Updated 'Access-Control-Allow-Origin' header in axiosApi and axiosPrivate instances in apiUtils.ts.

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -3,10 +3,14 @@ import axios, { AxiosInstance } from 'axios';
 
 export const axiosApi: AxiosInstance = axios.create({
   baseURL: API_URI,
+  headers: { 'Access-Control-Allow-Origin': true },
 });
 
 export const axiosPrivate: AxiosInstance = axios.create({
   baseURL: API_URI,
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': true,
+  },
   withCredentials: true,
 });
